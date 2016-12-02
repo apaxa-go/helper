548c03d894a3d27cced3134cb06219f31cfcfe02
apaxa-io <bav@wwww4.ru>
2016-12-02 18:46:31 +0300

    Add GetLine, GetFirstLine, ExtractLine in stringsh.

--- stringsh/strings.go
+++ stringsh/strings.go
@@ -1,7 +1,10 @@
 // Package stringsh is helper for system package strings.
 package stringsh
 
-import "golang.org/x/text/unicode/norm"
+import (
+	"golang.org/x/text/unicode/norm"
+	"strings"
+)
 
 // Len returns number of glyph in UTF-8 encoded string.
 func Len(s string) (l int) {
@@ -10,6 +13,41 @@ func Len(s string) (l int) {
 	for !ia.Done() {
 		l++
 		ia.Next()
+	}
+	return
+}
+
+// GetLine returns first line from s and position in s of estimated part.
+// Line delimiter may be "\n" or "\r\n". In both cases delimiter does not include nor in line nor in pos (pos points to first byte of second line).
+// If s does not contain delimiter than GetLine returns s as first line.
+// If there is no second line in s (s does not contain delimiter or there is no other bytes after delimiter) than pos will be point to non existing position in s.
+func GetLine(s string) (line string, pos int) {
+	i := strings.Index(s, "\n")
+	if i == -1 {
+		return s, len(s)
+	}
+
+	if i > 0 && s[i-1] == '\r' {
+		return s[:i-1], i + 1
+	}
+
+	return s[:i], i + 1
+}
+
+// GetFirstLine is a shortcut for GetLine but returning only first line.
+// As line delimiter does not include in result it may be hard to manipulate with estimated string.
+func GetFirstLine(s string) string {
+	line, _ := GetLine(s)
+	return line
+}
+
+// ExtractLine returns first line from s and estimated part of s.
+// Line delimiter is the same as in GetLine.
+// Also as in GetLine delimiter does not include nor in line nor in est.
+func ExtractLine(s string) (line, est string) {
+	line, pos := GetLine(s)
+	if pos < len(s) {
+		est = s[pos:]
 	}
 	return
 }
--- stringsh/strings_test.go
+++ stringsh/strings_test.go
@@ -1,6 +1,9 @@
 package stringsh
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
 
 func TestLen(t *testing.T) {
 	type testElement struct {
@@ -21,6 +24,62 @@ func TestLen(t *testing.T) {
 	for _, v := range tests {
 		if r := Len(v.s); r != v.l {
 			t.Errorf("string '%v', expected: %v, got: %v", v.s, v.l, r)
+		}
+	}
+}
+
+type firstLineTestElement struct {
+	s string
+	l string
+	p int
+	e string
+}
+
+var firstLineTests = []firstLineTestElement{
+	{"", "", 0, ""},
+	{"\r", "\r", 1, ""},
+	{"\n", "", 1, ""},
+	{"\r\n", "", 2, ""},
+	{"1\n", "1", 2, ""},
+	{"1\r", "1\r", 2, ""},
+	{"1\r\n", "1", 3, ""},
+	{"1\n2", "1", 2, "2"},
+	{"1\r2", "1\r2", 3, ""},
+	{"1\r\n2", "1", 3, "2"},
+	{"\n2", "", 1, "2"},
+	{"\r\n2", "", 2, "2"},
+	{"\r2", "\r2", 2, ""},
+	{"12\n34\n56\r\n78\r90", "12", 3, "34\n56\r\n78\r90"},
+	{"12\r\n34\n56\r\n78\r90", "12", 4, "34\n56\r\n78\r90"},
+	{"12\r34\n56\r\n78\r90", "12\r34", 6, "56\r\n78\r90"},
+}
+
+func TestGetLine(t *testing.T) {
+	for _, v := range firstLineTests {
+		if l, p := GetLine(v.s); l != v.l || p != v.p {
+			s := strings.Replace(v.s, "\n", "\\n", -1)
+			s = strings.Replace(s, "\r", "\\r", -1)
+			t.Errorf("%v: expect '%v' %v, got '%v' %v", s, v.l, v.p, l, p)
+		}
+	}
+}
+
+func TestGetFirstLine(t *testing.T) {
+	for _, v := range firstLineTests {
+		if l := GetFirstLine(v.s); l != v.l {
+			s := strings.Replace(v.s, "\n", "\\n", -1)
+			s = strings.Replace(s, "\r", "\\r", -1)
+			t.Errorf("%v: expect '%v', got '%v'", s, v.l, l)
+		}
+	}
+}
+
+func TestExtractLine(t *testing.T) {
+	for _, v := range firstLineTests {
+		if l, e := ExtractLine(v.s); l != v.l || e != v.e {
+			s := strings.Replace(v.s, "\n", "\\n", -1)
+			s = strings.Replace(s, "\r", "\\r", -1)
+			t.Errorf("%v: expect '%v' '%v', got '%v' '%v'", s, v.l, v.e, l, e)
 		}
 	}
 }
