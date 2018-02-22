9a246f4d4642313bcb85c267e17ed08655a6d3e2
unsacrificed <unsacrificed@wwww4.ru>
2018-02-22 19:06:34 +0300

    Init docs for bidih.

---
+++ unicodeh/bidih/doc.go
@@ -0,0 +1,5 @@
+// Package bidih implements Unicode bidirectional algorithm (TR9) also known as BIDI.
+// For more information see http://unicode.org/reports/tr9 .
+// Currently this package under development and is not ready for production use!!!
+package bidih
+
